45c38533a19b2bfdb4193a7f4aa2cd61baeeb6c6
Hasan Ugur <hasan.ugur@merena.com.tr>
2020-08-06 19:06:21 +0300

    Çarpınca yavaşlatma ve yeni uçak modeli eklendi

--- Tunnel Flight/Assets/Scripts/PlaneControl.cs
+++ Tunnel Flight/Assets/Scripts/PlaneControl.cs
@@ -2,157 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaneControl : MonoBehaviour
-{
-    [SerializeField]
-    private float speed = 500f;
+public class PlaneControl : MonoBehaviour {
+    public GameObject mainPlaneObject;
+    private float rotateSpeed = 25f;
+    private float thrust = 80f;
+    private Rigidbody rb;
+    private float minSpeed = 20f;
 
-    public float planeSpeed = 0.1f;
-    public float rotateSpeed = 0.03f;
-    private float xPos, yPos;
-
-    public Rigidbody rb;
-    public float maxSpeed = 50f;
-
-    public Transform topPoint, bottomPoint, leftPoint, rightPoint, topLeftPoint, topRightPoint, bottomLeftPoint, bottomRightPoint;
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+    void Start() {
+        rb = mainPlaneObject.GetComponent<Rigidbody>();
+        // rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float _xMov = Input.GetAxisRaw("Horizontal");
         float _yMov = Input.GetAxisRaw("Vertical");
 
-        Vector3 _movHorizontal = transform.right * _xMov;
-        Vector3 _movVertical = transform.up * _yMov;
-        Vector3 _moveForward = transform.forward * maxSpeed;
+        Vector3 _movHorizontal = rb.transform.right * _xMov;
+        Vector3 _movVertical = rb.transform.up * _yMov;
 
         //Final movement vector
-        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * rotateSpeed;
 
         rb.MovePosition(rb.position + _velocity * Time.deltaTime);
 
-        /*Vector3 planePos = this.transform.position;
-        xPos = 0f;
-        yPos = 0f;
-
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
-        {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                //this.transform.Rotate(Vector3.right, -1f);
-
-                //this.transform.Translate(planePos.x, rotateSpeed, planePos.z);
-                yPos = rotateSpeed;
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                //this.transform.Translate(planePos.x, -rotateSpeed, planePos.z);
-                //this.transform.Rotate(Vector3.right, 1f);
-
-                yPos = -rotateSpeed;
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                //this.transform.Rotate(Vector3.up, 1f);
-
-                //this.transform.Translate(rotateSpeed, planePos.y, planePos.z);
-                xPos = rotateSpeed;
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                //this.transform.Rotate(Vector3.up, -1f);
-
-                //this.transform.Translate(-rotateSpeed, planePos.y, planePos.z);
-                xPos = -rotateSpeed;
-            }
+        if (_xMov == 0 && _yMov == 0 && (rb.velocity.x != 0 || rb.velocity.y != 0)) {
+            rb.velocity = new Vector3(0f, 0f, rb.velocity.z);
         }
+        // Debug.Log("X_Mov: " + _xMov);
+        // Debug.Log("Y_Mov: " + _yMov);
+        // Debug.Log(rb.velocity);
 
-
-        this.transform.Translate(xPos, yPos, planeSpeed);
-        //this.transform.Translate(0, 0, planeSpeed);
-        */
-
-
-        /*if (rb.velocity.magnitude > maxSpeed)
-        {
-            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
-        }
-        rb.AddForce(0, 0, 0.1f, ForceMode.Impulse);
-        Debug.Log(rb.velocity.z);
-        */
     }
 
     void OnCollisionEnter(Collision col)
     {
-        /*Debug.Log("Çarptı.!!");
-        if (col.gameObject.tag == "PipeWall")
-        {
-            Debug.Log("Duvara Çarptı.!!");
-        }*/
+        // Debug.Log("Çarptı.!!");
+        // if (col.gameObject.tag == "PipeWall")
+        // {
+        //    Debug.Log("Duvara Çarptı.!!");
+        //    rb.AddForce(-transform.forward * thrust);
+        // //    rb.MovePosition(new Vector3(-20f, 0f, rb.position.z));
+        // }
 
-        ContactPoint[] contacts = new ContactPoint[10];
+    }
 
-        int numContacts = col.GetContacts(contacts);
-        for (int i = 0; i < numContacts; i++)
-        {
-            if (Vector3.Distance(contacts[i].point, topPoint.position) < 1f)
-            {
-                Debug.Log("Yukarı Çarptı.!!");
-                
-                rb.MovePosition(rb.position + new Vector3(0f, -5f, 0f));
+    private void OnCollisionStay(Collision col) {
+        Debug.Log("Touching..");
+        if (col.gameObject.tag == "PipeWall") {
+            this.rotateSpeed = 5f;
+            Debug.Log("Duvara Dokunuyor.!!");
+            if (rb.velocity.z > minSpeed) {
+                rb.AddForce(-transform.forward * thrust);
             }
-            if (Vector3.Distance(contacts[i].point, bottomPoint.position) < 1f)
-            {
-                Debug.Log("Aşağı Çarptı.!!");
-
-                rb.MovePosition(rb.position + new Vector3(0f, 5f, 0f));
-            }
-            if (Vector3.Distance(contacts[i].point, leftPoint.position) < 1f)
-            {
-                Debug.Log("Sola Çarptı.!!");
-
-                rb.MovePosition(rb.position + new Vector3(5f, 0f, 0f));
-            }
-            if (Vector3.Distance(contacts[i].point, rightPoint.position) < 1f)
-            {
-                Debug.Log("Sağa Çarptı.!!");
-
-                rb.MovePosition(rb.position + new Vector3(-5f, 0f, 0));
-            }
-            if (Vector3.Distance(contacts[i].point, topLeftPoint.position) < 1f)
-            {
-                Debug.Log("Sol Üste Çarptı.!!");
-
-                rb.MovePosition(rb.position + new Vector3(2.5f, -2.5f, 0));
-            }
-            if (Vector3.Distance(contacts[i].point, topRightPoint.position) < 1f)
-            {
-                Debug.Log("Sağ Üste Çarptı.!!");
-
-                rb.MovePosition(rb.position + new Vector3(-2.5f, -2.5f, 0));
-            }
-            if (Vector3.Distance(contacts[i].point, bottomLeftPoint.position) < 1f)
-            {
-                Debug.Log("Sol Alta Çarptı.!!");
-
-                rb.MovePosition(rb.position + new Vector3(2.5f, 2.5f, 0));
-            }
-            if (Vector3.Distance(contacts[i].point, bottomRightPoint.position) < 1f)
-            {
-                Debug.Log("Sağ Alta Çarptı.!!");
-
-                rb.MovePosition(rb.position + new Vector3(-2.5f, 2.5f, 0));
-            }
+           
         }
     }
 
-        protected void LateUpdate()
-    {
-        transform.localEulerAngles = new Vector3(0, 0, 0);
+    private void OnCollisionExit(Collision col) {
+        if (col.gameObject.tag == "PipeWall"){
+            this.rotateSpeed = 25f;
+        }
+    }
+
+    protected void LateUpdate() {
+        rb.transform.localEulerAngles = new Vector3(0, 0, 0);
     }
 }
--- Tunnel Flight/Assets/Scripts/PlaneForward.cs
+++ Tunnel Flight/Assets/Scripts/PlaneForward.cs
@@ -3,29 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaneForward : MonoBehaviour
-{
+public class PlaneForward : MonoBehaviour {
     public float forceMult = 2000;
     private Rigidbody rb;
-    private float normalSpeed;
+    private float normalSpeed = 80f;    
+    private float thrust = 10f;
 
     // Start is called before the first frame update
-    private void Awake()
-    {
+    private void Awake() {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * Time.deltaTime * forceMult;
-        normalSpeed = rb.velocity.z;
+        //rb.velocity = transform.forward * Time.deltaTime * forceMult;
+        //normalSpeed = rb.velocity.z;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         //Debug.Log(rb.velocity.z + " < " + normalSpeed);
-        if (rb.velocity.z < normalSpeed)
-        {
-            //Debug.Log("Fixed.!!!");
-            rb.velocity = transform.forward * Time.deltaTime * forceMult;
+        if (rb.velocity.z < normalSpeed) {
+            // Debug.Log("Add Force !!!");
+            //rb.velocity = transform.forward * Time.deltaTime * forceMult;
+            rb.AddForce(transform.forward * thrust);
         }
     }
+
 
 }
