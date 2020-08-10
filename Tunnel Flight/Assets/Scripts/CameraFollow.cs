7e21da28cb618a37620f9fa0e48e998345309018
Hasan Uğur <hasan.ugur4535@gmail.com>
2020-08-10 12:58:15 +0300

    Kamera Takip düzenlendi.

--- Tunnel Flight/Assets/Scripts/CameraFollow.cs
+++ Tunnel Flight/Assets/Scripts/CameraFollow.cs
@@ -7,12 +7,18 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     private float sSpeed = 1000.0f;
     private Vector3 dist = new Vector3(0, 2, -15);
+    private Vector3 moveLimit;
 
     private void Update() {
-        Vector3 dPos = target.position + dist;
+        moveLimit = new Vector3(PercentCalculator(transform.position.x, 20), PercentCalculator(transform.position.y, 20), 0);
+        Vector3 dPos = target.position + dist - moveLimit;
         Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
         transform.position = sPos;
         transform.LookAt(target.position);    
+    }
+
+    private float PercentCalculator(float val, int percent) {
+        return val * percent / 100;
     }
 
 }
